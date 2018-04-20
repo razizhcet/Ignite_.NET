ef154a54ffdaa65b72c23eaa87b7eb8232efc686
Razi Ahmad <razizhcet@gmail.com>
2018-04-20 20:14:33 +0530

    ignite c-sharp2

---
+++ IgniteC#/Ignite.NET1/Ignite.NET1/RemoteAssemblyLoading.cs
@@ -0,0 +1,36 @@
+﻿using Apache.Ignite.Core;
+using Apache.Ignite.Core.Compute;
+using Apache.Ignite.Core.Deployment;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ignite.NET1
+{
+    class RemoteAssemblyLoading
+    {
+        static void Main(string[] args)
+        {
+            var cfg = new IgniteConfiguration
+            {
+                PeerAssemblyLoadingMode = PeerAssemblyLoadingMode.CurrentAppDomain
+            };
+
+            using (var ignite = Ignition.Start(cfg))
+            {
+                ignite.GetCompute().Broadcast(new HelloAction());
+            }
+        }
+
+        class HelloAction : IComputeAction
+        {
+            public void Invoke()
+            {
+                Console.WriteLine("Hello, World!");
+                Console.ReadKey();
+            }
+        }
+    }
+}
