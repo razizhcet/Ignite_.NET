1ed3059887e7a1daa1b02f39fd8b7a1f3d2cf8a2
Razi Ahmad <razizhcet@gmail.com>
2018-04-21 19:10:57 +0530

    ignite .net3

---
+++ IgniteMultiPlatform/IgniteMultiPlatform/Message.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IgniteMultiPlatform
+{
+    class Message
+    {
+        public Message(string author, string text)
+        {
+            Author = author;
+            Text = text;
+        }
+
+        public string Author { get; }
+        public string Text { get; }
+    }
+}
